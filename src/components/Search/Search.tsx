9b36d8187dca84c605adf304ce0589379858fe3b
enrihg <99612570+enrihg@users.noreply.github.com>
2024-04-23 21:03:29 -0300

    search component improved

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,16 @@
 import './Search.css'
 import iconSearch from '../../assets/icon-search.svg'
+import { useState } from 'react'
 
 function Search() {
+    const [query, setQuery] = useState("");
+
     return (
         <search className='search'>
-            <form action="#">
+            <form action="#" onSubmit={(e) => e.preventDefault}>
                 <button><img src={iconSearch} alt="search icon" /></button>
                 <label htmlFor="search-input">Search</label>
-                <input id="search-input" type="search" placeholder="Search for movies or TV series"/>
+                <input id="search-input" type="search" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search for movies or TV series" />
             </form>
         </search>
     )
